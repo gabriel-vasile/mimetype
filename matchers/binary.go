3095f86f71890684ab509b0d5b92fc95c1333f4b
Anonymous <wxdxfg@hotmail.com>
2019-04-03 12:18:01 +0800

    support webassembly

--- matchers/binary.go
+++ matchers/binary.go
@@ -16,3 +16,9 @@ func Swf(in []byte) bool {
 		bytes.Equal(in[:3], []byte("FWS")) ||
 		bytes.Equal(in[:3], []byte("ZWS"))
 }
+
+// Wasm matches a web assembly File Format file.
+func Wasm(in []byte) bool {
+	return len(in) > 4 &&
+		bytes.Equal(in[:4], []byte{0x00, 0x61, 0x73, 0x6D})
+}
