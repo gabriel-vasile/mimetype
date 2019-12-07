465dcdab43169de0458a83c6ba73025cc9c1b4cb
Gabriel Vasile <vasile.gabriel@email.com>
2019-12-07 23:21:21 +0200

    Rename package files

--- mime.go
+++ mime.go
@@ -1,53 +1,94 @@
-// Package mimetype uses magic number signatures to detect the MIME type of a file.
-//
-// mimetype stores the list of MIME types in a tree structure with
-// "application/octet-stream" at the root of the hierarchy. The hierarchy
-// approach minimizes the number of checks that need to be done on the input
-// and allows for more precise results once the base type of file has been
-// identified.
 package mimetype
 
-import (
-	"io"
-	"os"
+import "mime"
 
-	"github.com/gabriel-vasile/mimetype/internal/matchers"
-)
-
-// Detect returns the MIME type found from the provided byte slice.
-// Failure to identify the format results in application/octet-stream
-// being returned.
-func Detect(in []byte) (mime *MIME) {
-	if len(in) == 0 {
-		return newMIME("inode/x-empty", "", matchers.True)
-	}
-
-	return root.match(in, root)
+// MIME represents a file format in the tree structure of formats.
+type MIME struct {
+	mime      string
+	aliases   []string
+	extension string
+	matchFunc func([]byte) bool
+	children  []*MIME
+	parent    *MIME
 }
 
-// DetectReader returns the MIME type of the provided reader.
-// The result is always a valid MIME type, with application/octet-stream
-// returned when identification failed with or without an error.
-func DetectReader(r io.Reader) (mime *MIME, err error) {
-	in := make([]byte, matchers.ReadLimit)
-	n, err := r.Read(in)
-	if err != nil && err != io.EOF {
-		return root, err
-	}
-	in = in[:n]
-
-	return Detect(in), nil
+// String returns the string representation of the MIME type, e.g., "application/zip".
+func (n *MIME) String() string {
+	return n.mime
 }
 
-// DetectFile returns the MIME type of the provided file.
-// The result is always a valid MIME type, with application/octet-stream
-// returned when identification failed with or without an error.
-func DetectFile(file string) (mime *MIME, err error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return root, err
-	}
-	defer f.Close()
+// Extension returns the file extension associated with the MIME type.
+// It includes the leading dot, as in ".html". When the file format does not
+// have an extension, the empty string is returned.
+func (n *MIME) Extension() string {
+	return n.extension
+}
 
-	return DetectReader(f)
+// Parent returns the parent MIME type from the tree structure.
+// Each MIME type has a non-nil parent, except for the root MIME type.
+func (n *MIME) Parent() *MIME {
+	return n.parent
+}
+
+// Is checks whether this MIME type, or any of its aliases, is equal to the
+// expected MIME type. MIME type equality test is done on the "type/subtype"
+// sections, ignores any optional MIME parameters, ignores any leading and
+// trailing whitespace, and is case insensitive.
+func (n *MIME) Is(expectedMIME string) bool {
+	// Parsing is needed because some detected MIME types contain parameters
+	// that need to be stripped for the comparison.
+	expectedMIME, _, _ = mime.ParseMediaType(expectedMIME)
+	found, _, _ := mime.ParseMediaType(n.mime)
+
+	if expectedMIME == found {
+		return true
+	}
+	for _, alias := range n.aliases {
+		if alias == expectedMIME {
+			return true
+		}
+	}
+
+	return false
+}
+
+func newMIME(mime, extension string, matchFunc func([]byte) bool, children ...*MIME) *MIME {
+	n := &MIME{
+		mime:      mime,
+		extension: extension,
+		matchFunc: matchFunc,
+		children:  children,
+	}
+
+	for _, c := range children {
+		c.parent = n
+	}
+
+	return n
+}
+
+func (n *MIME) alias(aliases ...string) *MIME {
+	n.aliases = aliases
+	return n
+}
+
+// match does a depth-first search on the matchers tree.
+// it returns the deepest successful matcher for which all the children fail.
+func (n *MIME) match(in []byte, deepestMatch *MIME) *MIME {
+	for _, c := range n.children {
+		if c.matchFunc(in) {
+			return c.match(in, c)
+		}
+	}
+
+	return deepestMatch
+}
+
+func (n *MIME) flatten() []*MIME {
+	out := []*MIME{n}
+	for _, c := range n.children {
+		out = append(out, c.flatten()...)
+	}
+
+	return out
 }
---
+++ mimetype.go
@@ -0,0 +1,53 @@
+// Package mimetype uses magic number signatures to detect the MIME type of a file.
+//
+// mimetype stores the list of MIME types in a tree structure with
+// "application/octet-stream" at the root of the hierarchy. The hierarchy
+// approach minimizes the number of checks that need to be done on the input
+// and allows for more precise results once the base type of file has been
+// identified.
+package mimetype
+
+import (
+	"io"
+	"os"
+
+	"github.com/gabriel-vasile/mimetype/internal/matchers"
+)
+
+// Detect returns the MIME type found from the provided byte slice.
+// Failure to identify the format results in application/octet-stream
+// being returned.
+func Detect(in []byte) (mime *MIME) {
+	if len(in) == 0 {
+		return newMIME("inode/x-empty", "", matchers.True)
+	}
+
+	return root.match(in, root)
+}
+
+// DetectReader returns the MIME type of the provided reader.
+// The result is always a valid MIME type, with application/octet-stream
+// returned when identification failed with or without an error.
+func DetectReader(r io.Reader) (mime *MIME, err error) {
+	in := make([]byte, matchers.ReadLimit)
+	n, err := r.Read(in)
+	if err != nil && err != io.EOF {
+		return root, err
+	}
+	in = in[:n]
+
+	return Detect(in), nil
+}
+
+// DetectFile returns the MIME type of the provided file.
+// The result is always a valid MIME type, with application/octet-stream
+// returned when identification failed with or without an error.
+func DetectFile(file string) (mime *MIME, err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return root, err
+	}
+	defer f.Close()
+
+	return DetectReader(f)
+}
