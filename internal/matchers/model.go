2816c4730920ad9b356a4763bf939c64e0ce7710
Kaiming Guo <augustin.guo@gmail.com>
2021-01-25 18:47:52 +0800

    Add glb format support
    GLB is the binary file format representation of 3D models saved in the
    GL Transmission Format (glTF). The format uses `model/gltf-binary` as
    MIME type.
    
    Reference:
    - https://docs.fileformat.com/3d/glb/
    - https://www.iana.org/assignments/media-types/model/gltf-binary
    
    Replace testdata
    
    Use smaller 3D sample file.

---
+++ internal/matchers/model.go
@@ -0,0 +1,25 @@
+package matchers
+
+import "bytes"
+
+// Glb matches a glTF model format file.
+func Glb(in []byte) bool {
+	// GLB is the binary file format representation of 3D models save in
+	// the GL transmission Format (glTF).
+	// see more: https://docs.fileformat.com/3d/glb/
+	//           https://www.iana.org/assignments/media-types/model/gltf-binary
+	if len(in) < 8 {
+		return false
+	}
+
+	// GLB file format is based on little endian and its header structure
+	// show  below:
+	//
+	// <-- 12-byte header                             -->
+	// | magic            | version          | length   |
+	// | (uint32)         | (uint32)         | (uint32) |
+	// | \x67\x6C\x54\x46 | \x01\x00\x00\x00 | ...      |
+	// | g   l   T   F    | 1                | ...      |
+	return bytes.HasPrefix(in, []byte("\x67\x6C\x54\x46\x02\x00\x00\x00")) ||
+		bytes.HasPrefix(in, []byte("\x67\x6C\x54\x46\x01\x00\x00\x00"))
+}
