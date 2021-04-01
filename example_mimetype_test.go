3b55af923958dce1369d33565b5257211dc40ecd
Gabriel Vasile <vasile.gabriel@email.com>
2021-04-01 12:38:01 +0300

    Improve reusable reader example

--- example_mimetype_test.go
+++ example_mimetype_test.go
@@ -3,15 +3,13 @@ package mimetype_test
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/gabriel-vasile/mimetype"
 )
 
 func Example_detect() {
-	testBytes := []byte("This random text should have a MIME type of text/plain; charset=utf-8.")
+	testBytes := []byte("This random text has a MIME type of text/plain; charset=utf-8.")
 
 	mime := mimetype.Detect(testBytes)
 	fmt.Println(mime.Is("text/plain"), mime.String(), mime.Extension())
@@ -30,7 +28,7 @@ func Example_detect() {
 // a text file", they can differentiated by searching for the text/plain MIME
 // in their MIME hierarchy.
 func Example_textVsBinary() {
-	testBytes := []byte("This random text should have a MIME type of text/plain; charset=utf-8.")
+	testBytes := []byte("This random text has a MIME type of text/plain; charset=utf-8.")
 	detectedMIME := mimetype.Detect(testBytes)
 
 	isBinary := true
@@ -45,7 +43,7 @@ func Example_textVsBinary() {
 }
 
 func Example_whitelist() {
-	testBytes := []byte("This random text should have a MIME type of text/plain; charset=utf-8.")
+	testBytes := []byte("This random text has a MIME type of text/plain; charset=utf-8.")
 	allowed := []string{"text/plain", "application/zip", "application/pdf"}
 	mime := mimetype.Detect(testBytes)
 
@@ -55,33 +53,4 @@ func Example_whitelist() {
 		fmt.Printf("%s is now allowed\n", mime)
 	}
 	// Output: text/plain; charset=utf-8 is allowed
-}
-
-// When detecting from an io.Reader, mimetype will read the header of the input.
-// This means the reader cannot just be reused (to save the file, for example)
-// because the header is now missing from the reader.
-//
-// If the input is a pure io.Reader, use io.TeeReader, io.MultiReader and bytes.Buffer
-// to create a new reader containing the whole unaltered data.
-//
-// If the input is an io.ReadSeeker, call reader.Seek(0, io.SeekStart) to rewind it.
-func Example_reusableReader() {
-	// Set header size to 10 bytes for this example.
-	mimetype.SetLimit(10)
-
-	testBytes := []byte("This random text should have a MIME type of text/plain; charset=utf-8.")
-	inputReader := bytes.NewReader(testBytes)
-
-	// buf will store the 10 bytes mimetype used for detection.
-	header := bytes.NewBuffer(nil)
-
-	// After DetectReader, the first 10 bytes are stored in buf.
-	mime, err := mimetype.DetectReader(io.TeeReader(inputReader, header))
-
-	// Concatenate back the first 10 bytes.
-	// reusableReader now contains the complete, original data.
-	reusableReader := io.MultiReader(header, inputReader)
-	text, _ := ioutil.ReadAll(reusableReader)
-	fmt.Println(mime, bytes.Equal(testBytes, text), err)
-	// Output: text/plain; charset=utf-8 true <nil>
 }
---
+++ example_reader_test.go
@@ -0,0 +1,46 @@
+package mimetype_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/gabriel-vasile/mimetype"
+)
+
+// Pure io.Readers (meaning those without a Seek method) cannot be read twice.
+// This means that once DetectReader has been called on an io.Reader, that reader
+// is missing the bytes representing the header of the file.
+// To detect the MIME type and then reuse the input, use a buffer, io.TeeReader,
+// and io.MultiReader to create a new reader containing the original, unaltered data.
+//
+// If the input is an io.ReadSeeker instead, call input.Seek(0, io.SeekStart)
+// before reusing it.
+func Example_detectReader() {
+	testBytes := []byte("This random text has a MIME type of text/plain; charset=utf-8.")
+	input := bytes.NewReader(testBytes)
+
+	mime, recycledInput, err := recycleReader(input)
+
+	// Verify recycledInput contains the original input.
+	text, _ := ioutil.ReadAll(recycledInput)
+	fmt.Println(mime, bytes.Equal(testBytes, text), err)
+	// Output: text/plain; charset=utf-8 true <nil>
+}
+
+// recycleReader returns the MIME type of input and a new reader
+// containing the whole data from input.
+func recycleReader(input io.Reader) (mimeType string, recycled io.Reader, err error) {
+	// header will store the bytes mimetype uses for detection.
+	header := bytes.NewBuffer(nil)
+
+	// After DetectReader, the data read from input is copied into header.
+	mime, err := mimetype.DetectReader(io.TeeReader(input, header))
+
+	// Concatenate back the header to the rest of the file.
+	// recycled now contains the complete, original data.
+	recycled = io.MultiReader(header, input)
+
+	return mime.String(), recycled, err
+}
