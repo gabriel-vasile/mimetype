9fdffeb8b24585ba997a829717430c82995c6608
Gabriel Vasile <gabriel.vasile@email.com>
2021-11-11 23:48:26 +0200

    Fix JSON returned for partial documents when readLimit=0
    For #198

--- internal/magic/text.go
+++ internal/magic/text.go
@@ -155,12 +155,14 @@ func Php(raw []byte, limit uint32) bool {
 // JSON matches a JavaScript Object Notation file.
 func JSON(raw []byte, limit uint32) bool {
 	raw = trimLWS(raw)
-	if len(raw) == 0 || (raw[0] != '[' && raw[0] != '{') {
+	// #175 A single JSON string, number or bool is not considered JSON.
+	// JSON objects and arrays are reported as JSON.
+	if len(raw) < 2 || (raw[0] != '[' && raw[0] != '{') {
 		return false
 	}
 	parsed, err := json.Scan(raw)
 	// If the full file content was provided, check there is no error.
-	if len(raw) < int(limit) {
+	if limit == 0 || len(raw) < int(limit) {
 		return err == nil
 	}
 
