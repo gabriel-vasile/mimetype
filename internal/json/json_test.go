834b1aa2c6a90b4e8fb9f42ffb79b4838b4e17f9
Gabriel Vasile <gabriel.vasile@email.com>
2021-11-15 19:23:41 +0200

    Move test cases inside test func

--- internal/json/json_test.go
+++ internal/json/json_test.go
@@ -6,26 +6,25 @@ package json
 
 import "testing"
 
-var scanTests = []struct {
-	data   string
-	length int
-	ok     bool
-}{
-	{`foo`, 2, false},
-	{`}{`, 1, false},
-	{`{]`, 2, false},
-	{`{}`, 2, true},
-	{`{"foo":"bar"}`, 13, true},
-	{`{"foo":"21\t\u0009 \u1234","bar":{"baz":["qux"]}`, 48, false},
-	{`{"foo":"bar","bar":{"baz":["qux"]}}`, 35, true},
-	{`{"foo":-1,"bar":{"baz":[true, false, null, 100, 0.123]}}`, 56, true},
-	{`{"foo":-1,"bar":{"baz":[tru]}}`, 28, false},
-	{`{"foo":-1,"bar":{"baz":[nul]}}`, 28, false},
-	{`{"foo":-1,"bar":{"baz":[314e+1]}}`, 33, true},
-}
-
 func TestScan(t *testing.T) {
-	for _, st := range scanTests {
+	tCases := []struct {
+		data   string
+		length int
+		ok     bool
+	}{
+		{`foo`, 2, false},
+		{`}{`, 1, false},
+		{`{]`, 2, false},
+		{`{}`, 2, true},
+		{`{"foo":"bar"}`, 13, true},
+		{`{"foo":"21\t\u0009 \u1234","bar":{"baz":["qux"]}`, 48, false},
+		{`{"foo":"bar","bar":{"baz":["qux"]}}`, 35, true},
+		{`{"foo":-1,"bar":{"baz":[true, false, null, 100, 0.123]}}`, 56, true},
+		{`{"foo":-1,"bar":{"baz":[tru]}}`, 28, false},
+		{`{"foo":-1,"bar":{"baz":[nul]}}`, 28, false},
+		{`{"foo":-1,"bar":{"baz":[314e+1]}}`, 33, true},
+	}
+	for _, st := range tCases {
 		scanned, err := Scan([]byte(st.data))
 		if scanned != st.length {
 			t.Errorf("Scan length error: expected: %d; got: %d; input: %s",
