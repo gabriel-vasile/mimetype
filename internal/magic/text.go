f1f2cbd3a152940ddb41bd999d77a0c155a553a6
Dovydas Joksas <dovydas@yoshke.org>
2021-12-19 20:50:35 +0000

    Ensure that beginning of a line in `regexPrefix`

--- internal/magic/text.go
+++ internal/magic/text.go
@@ -121,7 +121,7 @@ var (
 	// Rtf matches a Rich Text Format file.
 	Rtf = prefix([]byte("{\\rtf1"))
 	// Srt matches a SubRip file.
-	Srt = regexPrefix("^1\n[0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3} --> [0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3}")
+	Srt = regexPrefix("1\n[0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3} --> [0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3}")
 	// Vtt matches a Web Video Text Tracks (WebVTT) file.
 	Vtt = prefix([]byte("WEBVTT"))
 )
