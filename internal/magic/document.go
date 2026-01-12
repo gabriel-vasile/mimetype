68fcbe0926fe5dbcecfa8b9b1237b41d579f5c03
Gabriel Vasile <gabriel.vasile@email.com>
2026-01-12 23:07:18 +0900

    framemaker: add support

--- internal/magic/document.go
+++ internal/magic/document.go
@@ -3,6 +3,8 @@ package magic
 import (
 	"bytes"
 	"encoding/binary"
+
+	"github.com/gabriel-vasile/mimetype/internal/scan"
 )
 
 // Pdf matches a Portable Document Format file.
@@ -107,4 +109,17 @@ func Inf(raw []byte, _ uint32) bool {
 // Hlp matches an OS/2 .hlp file.
 func Hlp(raw []byte, _ uint32) bool {
 	return bytes.HasPrefix(raw, []byte("HSP\x10\x9b\x00"))
+}
+
+// FrameMaker matches an Adobe FrameMaker file.
+func FrameMaker(raw []byte, _ uint32) bool {
+	b := scan.Bytes(raw)
+	if !(bytes.HasPrefix(b, []byte("<MakerFile")) ||
+		bytes.HasPrefix(b, []byte("<MakerDictionary")) ||
+		b.Match([]byte("<BOOKFILE"), scan.IgnoreCase) != -1) {
+		return false
+	}
+
+	// To avoid plain text false positives.
+	return bytes.IndexByte(b[:min(len(b), 512)], 0x00) != -1
 }
